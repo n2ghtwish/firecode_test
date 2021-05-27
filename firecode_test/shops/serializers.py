e643d54509973fd5c0266d992180c36cc6cac69e
Alexander Pobezhimov <thenewpapakarlo@gmail.com>
2021-05-28 01:19:09 +0300

    Исправлены классы сериализаторов

--- firecode_test/shops/serializers.py
+++ firecode_test/shops/serializers.py
@@ -5,22 +5,22 @@ from rest_framework import serializers
 from .models import Shop
 
 
-class CitySerializer(serializers.Serializer, ABC):
+class CitySerializer(serializers.Serializer):
     name = serializers.CharField(max_length=255)
 
 
-class StreetSerializer(serializers.Serializer, ABC):
+class StreetSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=255)
     city = serializers.CharField(max_length=255)
 
 
-class ShopSerializer(serializers.Serializer, ABC):
+class ShopSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=255)
     street = serializers.CharField(max_length=255)
     city = serializers.CharField(max_length=255)
 
 
-class AddShopSerializer(serializers.Serializer, ABC):
+class AddShopSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=255)
     street_id = serializers.IntegerField()
     city_id = serializers.IntegerField()
